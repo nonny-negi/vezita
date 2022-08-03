f8962a92ba04d7f29911d80c1cd4c1173e5f8592
Nonny <codewithonny@gmail.com>
2022-08-03 11:59:45 +0530

    user model fix

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -23,8 +23,8 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: [true, "Please Enter Your Password"],
-    minLength: [8, "Password should be greater than 8 characters"],
+    // required: [true, "Please Enter Your Password"],
+    // minLength: [8, "Password should be greater than 8 characters"],
     select: false,
   },
   isEmailVerified: {
